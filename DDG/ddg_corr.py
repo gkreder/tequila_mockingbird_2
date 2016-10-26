7ff712fe7257ef8f7c65d03ba4d2ef21386e9df9
gkreder <gkreder@gmail.com>
2016-10-26 00:22:27 -0700

    DDG Correlation scores OTSU
    Added script and two output pkl files (one for each replicate). Each
    pkl file contains a dict. Keys are locations on Ub, values are the r
    values calculated for each position.
    
    For each position we have some mutations and their corresponding DDG
    affinity values (each value corresponds to a single AA mutation at a
    position). We also have a fitness scores for a AA mutation at the given
    position. So for each position, take all the mutations for which we
    have both a fitness score and a DDG value, then plot them against each
    other and find the best fit linear line and r value.

--- DDG/ddg_corr.py
+++ DDG/ddg_corr.py
@@ -1,11 +1,70 @@
 import csv
+import pickle
+import scipy.stats
 
 
 
-ddg_affinities = []
+ddg_affinities_OTU = {}
 with open('uby_OTU.tsv', 'rb') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter='\t')
-	for row in spamreader:
-		print row[3]
-	if row[3] != 'None':
-		ddg_affinities.append(float(row[3]))
+	for row_number, row in enumerate(spamreader):
+		if row_number == 0:
+			continue
+		AA = row[2].split(' ')[1][-1]
+		pos = int(row[2].split(' ')[1][1 : -1])
+		index_tup = (pos, AA)
+		# print index_tup
+		# print row[3]
+		if row[3] != 'None':
+			ddg_affinities_OTU[index_tup] = float(row[3])
+
+
+r1_differences_scores = pickle.load(open('../fitness_scores/jupyter/input_output_files/output/r1_differences_scores.pkl', 'rb'))
+r2_differences_scores = pickle.load(open('../fitness_scores/jupyter/input_output_files/output/r2_differences_scores.pkl', 'rb'))
+
+common_keys_r1 = set(ddg_affinities_OTU.keys()).intersection(set(r1_differences_scores.keys()))
+common_keys_r2 = set(ddg_affinities_OTU.keys()).intersection(set(r2_differences_scores.keys()))
+
+
+r1_corr_scores_temp = {}
+for pos, AA in common_keys_r1:
+	key = (pos, AA)
+	if pos in r1_corr_scores_temp:
+		r1_corr_scores_temp[pos]['ddgs'].append(ddg_affinities_OTU[key])
+		r1_corr_scores_temp[pos]['scores'].append(r1_differences_scores[key])
+	else:
+		r1_corr_scores_temp[pos] = {'ddgs' : [ddg_affinities_OTU[key]], 'scores' : [r1_differences_scores[key]]}
+
+r2_corr_scores_temp = {}
+for pos, AA in common_keys_r2:
+	key = (pos, AA)
+	if pos in r2_corr_scores_temp:
+		r2_corr_scores_temp[pos]['ddgs'].append(ddg_affinities_OTU[key])
+		r2_corr_scores_temp[pos]['scores'].append(r2_differences_scores[key])
+	else:
+		r2_corr_scores_temp[pos] = {'ddgs' : [ddg_affinities_OTU[key]], 'scores' : [r2_differences_scores[key]]}
+
+r1_corr_scores = {}
+for pos in r1_corr_scores_temp.keys():
+	# key = (pos, AA)
+	temp_x = r1_corr_scores_temp[pos]['scores']
+	temp_y = r1_corr_scores_temp[pos]['ddgs']
+	slope, intercept, rvalue, pvalue, stderr = scipy.stats.linregress(temp_x, temp_y)
+	r1_corr_scores[pos] = rvalue
+pickle.dump(r1_corr_scores, open('./output/r1_ddg_affinity_OTSU_corr.pkl', 'wb'))
+
+r2_corr_scores = {}
+for pos in r2_corr_scores_temp.keys():
+	# key = (pos, AA)
+	temp_x = r2_corr_scores_temp[pos]['scores']
+	temp_y = r2_corr_scores_temp[pos]['ddgs']
+	slope, intercept, rvalue, pvalue, stderr = scipy.stats.linregress(temp_x, temp_y)
+	r2_corr_scores[pos] = rvalue
+pickle.dump(r2_corr_scores, open('./output/r2_ddg_affinity_OTSU_corr.pkl', 'wb'))
+
+# print r2_corr_scores
+	# print pos
+
+
+
+
